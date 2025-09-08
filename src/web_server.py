a5deba88551938e1aabe74a85262a8cbfb04c113
Pierre SCHWEITZER <pierre.schweitzer@epita.fr>
2025-09-08 15:19:15 +0200

    fix: prediction body (level1)

--- src/web_server.py
+++ src/web_server.py
@@ -6,7 +6,7 @@ import pandas as pd
 app = FastAPI(port=5757)
 model = joblib.load("../model/regression.joblib")
 
-class Item(BaseModel):
+class PredictionBody(BaseModel):
     size: float
     nb_rooms: int
     garden: int
@@ -16,7 +16,7 @@ async def get_predict():
     return {"y_pred": 2}
 
 @app.post("/predict")
-async def predict_pricing(item: Item):
+async def predict_pricing(item: PredictionBody):
     df = pd.DataFrame(
             [[item.size, item.nb_rooms, item.garden]],
             columns=['size', 'nb_rooms', 'garden']
